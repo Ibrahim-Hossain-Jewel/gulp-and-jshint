5bccb90de1011ff5646035bc4ea30af16e89151f
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2021-12-27 10:32:15 +0600

    jshint before use in a specific task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const { task, src, dest, watch, series } = require("gulp");
 const babel = require("gulp-babel");
+const jshint = require("gulp-jshint");
+
 
 task("js",()=>{
-    return src("src/*.js").pipe(babel()).pipe(dest("dist/js"));
+    return src("src/*.js").pipe(babel()).pipe(dest("dist"));
 })
 task("moveHTML",()=>{
     return src("src/*.html").pipe(dest("dist"));
 });
 task("watch",()=>{
     watch("src/*.js",{persistent: false},series("js"));
-}); 
+});
+task("lint",()=>{
+    return src("src/*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"));
+});
 task("default",series('moveHTML','js'));
 //you don't have to add the watch task inside the series() API and also you could delete the persistent object property
 
